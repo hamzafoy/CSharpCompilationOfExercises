c9469f43ddab5d7bf531e53e3afb34be82ec6ace
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-04 15:29:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * This repository stores completed code exercises from csharpexercises.com and
+ * from Exercism.
+ */
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Expected behavior of AddAndMultiply() \nAdd the first two integers and multiply its result with the third integer: ");
+        Console.WriteLine(AddAndMultiply(3, 5, 9));
+    }
+
+    private static int AddAndMultiply(int firstInteger, int secondInteger, int thirdInteger)
+    {
+        return (firstInteger + secondInteger) * thirdInteger;
+    }
+}
