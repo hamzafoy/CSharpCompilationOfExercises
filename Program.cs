151ea5b443d4444c1b8d306b8a5aedba3af6cc97
hamzafoy <hamzaoflouisville@gmail.com>
2022-02-04 21:18:30 -0500

    Added additional methods demonstrating use of conditional statements and `char` type methods.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,12 @@ public class Program
         Console.Write("\n");
         Console.Write("Expected behavior of ElementaryOperations() \nAdd, subtract, multiply, & divide two numbers, return error message if you try to divide by 0: ");
         Console.WriteLine(ElementaryOperations(5, 12));
+        Console.Write("\n");
+        Console.Write("Expected behavior of AddOrDivideBasedOnModulo() \nIf the 1st & 2nd inputs are divisible by 2 or 3, their product are printed - otherwise their sum: ");
+        Console.WriteLine(AddOrDivideBasedOnModulo(7, 12));
+        Console.Write("\n");
+        Console.Write("Expected behavior of IsThisAllUppercaseLetters() \nIf the 3-letter string contains all Uppercase letters, return true: ");
+        Console.WriteLine(IsThisAllUppercaseLetters("ZT!"));
     }
 
     private static int AddAndMultiply(int firstInteger, int secondInteger, int thirdInteger)
@@ -39,5 +45,14 @@ public class Program
         //This use of a decimal type is to allow for quotients to be rounded up to the 5th place to avoid long decimal values being rendered.
         decimal quotient = Decimal.Round(Convert.ToDecimal(divideByZeroCheck),5);
         return $"{firstInteger + secondInteger}, {firstInteger - secondInteger}, {firstInteger * secondInteger}, {quotient}";
+    }
+    private static int AddOrDivideBasedOnModulo(int firstInteger, int secondInteger)
+    {
+        int result = ((firstInteger % 2 == 0 || firstInteger % 3 == 0) && (secondInteger % 2 == 0 || secondInteger % 3 == 0)) ? (firstInteger * secondInteger) : (firstInteger + secondInteger);
+        return result;
+    }
+    private static bool IsThisAllUppercaseLetters(string a)
+    {
+        return (char.IsUpper(a[0]) && char.IsUpper(a[1]) && char.IsUpper(a[2]));
     }
 }
